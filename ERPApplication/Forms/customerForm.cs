30abb587248e80413e67e05ae683fe59f5af581e
Mauro Rogledi <mauro.rogledi@github.com>
2018-02-28 18:33:20 +0100

    Start customerForms

---
+++ ERPApplication/Forms/customerForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ERPCustomer.Forms
+{
+public partial class customerForm : ERPFramework.Forms.DocumentForm
+{
+    public customerForm()
+    {
+        InitializeComponent();
+    }
+}
+}
---
+++ ERPApplication/ModuleData/DBManagers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ERPFramework.Data;
+using ERPFramework.Forms;
+
+namespace ERPCustomer.ModuleData
+{
+    #region DBManagerCustomer
+internal class DBManagerCustomer : DBManager
+{
+    public DBManagerCustomer(string name, IDocument document)
+        : base(name, document)
+    { }
+
+    protected override string CreateMasterQuery(ref List<SqlABParameter> dParam)
+    {
+        var qB = new QueryBuilder();
+
+        return qB.SelectAllFrom<CustomerTable>().
+            Where(CustomerTable.ID).IsEqualTo(dParam[0]).
+            Query;
+    }
+
+    protected override List<SqlABParameter> CreateMasterParam()
+    {
+        var PList = new List<SqlABParameter>();
+
+        var sParam1 = new SqlABParameter("@p1", CustomerTable.ID) { Value = CustomerTable.ID.DefaultValue };
+        PList.Add(sParam1);
+        return PList;
+    }
+
+    protected override void SetParameters(ERPFramework.Controls.IRadarParameters key, DBCollection collection)
+    {
+        collection.Parameter[0].Value = key[0];
+    }
+}
+    #endregion
+}
