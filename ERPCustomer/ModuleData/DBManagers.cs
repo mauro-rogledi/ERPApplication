3461f33f9151e8039d92ea6bfe366b36820d88d3
Mauro Rogledi <mauro.rogledi@github.com>
2018-06-22 17:22:37 +0200

    before merge

--- ERPCustomer/ModuleData/DBManagers.cs
+++ ERPCustomer/ModuleData/DBManagers.cs
@@ -3,40 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ERPFramework.Controls;
 using ERPFramework.Data;
 using ERPFramework.Forms;
 
 namespace ERPCustomer.ModuleData
 {
     #region DBManagerCustomer
-internal class DBManagerCustomer : DBManager
-{
-    public DBManagerCustomer(string name, IDocument document)
-        : base(name, document)
-    { }
-
-    protected override string CreateMasterQuery(ref List<SqlProxyParameter> dParam)
+    internal class DBManagerCustomer : DBManager
     {
-        var qB = new QueryBuilder();
+        public DBManagerCustomer(string name, IDocument document)
+            : base(name, document)
+        { }
 
-        return qB.SelectAllFrom<CustomerTable>().
-            Where(CustomerTable.ID).IsEqualTo(dParam[0]).
-            Query;
+        protected override string CreateMasterQuery(SqlProxyParameterCollection dParam)
+        {
+            var qB = new QueryBuilder();
+
+            return qB.SelectAllFrom<CustomerTable>().
+                Where(CustomerTable.ID).IsEqualTo(dParam["@p1"]).
+                Query;
+        }
+
+        protected override void CreateMasterParam(SqlProxyParameterCollection parameters)
+        {
+            parameters.Add(
+                new SqlProxyParameter("@p1", CustomerTable.ID)
+                );
+        }
+
+        protected override void SetParameters(IRadarParameters key, DataAdapterProperties collection)
+        {
+            collection.Parameters["@p1"].Value = key[CustomerTable.ID];
+        }
     }
-
-    protected override List<SqlProxyParameter> CreateMasterParam()
-    {
-        var PList = new List<SqlProxyParameter>();
-
-        var sParam1 = new SqlProxyParameter("@p1", CustomerTable.ID) { Value = CustomerTable.ID.DefaultValue };
-        PList.Add(sParam1);
-        return PList;
-    }
-
-    protected override void SetParameters(ERPFramework.Controls.IRadarParameters key, DBCollection collection)
-    {
-        collection.Parameter[0].Value = key[0];
-    }
-}
     #endregion
 }
